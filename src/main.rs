003de736d9471902f30fc507099d07885cc74ea2
shuimuliang <shuimuliang@gmail.com>
2023-12-15 22:58:10 +0800

    feat: LED PB0, PB1 toggle

--- src/main.rs
+++ src/main.rs
@@ -28,15 +28,18 @@ fn main() -> ! {
     let ccdr = rcc.sys_ck(200.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
     // 设置LED对应的GPIO
-    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
-    let mut led = gpioe.pe3.into_push_pull_output();
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    // PB0 绿色
+    // PB1 红色
+    let mut _led0 = gpiob.pb0.into_push_pull_output();
+    let mut _led1 = gpiob.pb1.into_push_pull_output();
 
     // cortex-m已经实现好了delay函数，直接拿到，下面使用
     let mut delay = cp.SYST.delay(ccdr.clocks);
 
     loop {
         // 点灯并且输出RTT日志
-        led.toggle();
+        // led.toggle();
         rprintln!("Hello World!");
         // 延时500ms
         delay.delay_ms(500_u16);
