5f46b6fe8020a22c4bcc8e50c11e75482ea143e5
shuimuliang <shuimuliang@gmail.com>
2023-12-15 23:03:36 +0800

    feat: back to empty main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,32 +16,9 @@ use stm32h7xx_hal::{pac, prelude::*};
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    // 获取cortex核心外设和stm32h7的所有外设
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
-
-    // Power 设置
-    let pwr = dp.PWR.constrain();
-    let pwrcfg = pwr.freeze();
-    // 初始化RCC
-    let rcc = dp.RCC.constrain();
-    let ccdr = rcc.sys_ck(200.MHz()).freeze(pwrcfg, &dp.SYSCFG);
-
-    // 设置LED对应的GPIO
-    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
-    // PB0 绿色
-    // PB1 红色
-    let mut _led0 = gpiob.pb0.into_push_pull_output();
-    let mut _led1 = gpiob.pb1.into_push_pull_output();
-
-    // cortex-m已经实现好了delay函数，直接拿到，下面使用
-    let mut delay = cp.SYST.delay(ccdr.clocks);
+    rprintln!("Hello World!");
 
     loop {
-        // 点灯并且输出RTT日志
-        // led.toggle();
-        rprintln!("Hello World!");
         // 延时500ms
-        delay.delay_ms(500_u16);
     }
 }
