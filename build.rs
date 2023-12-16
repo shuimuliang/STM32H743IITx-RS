cf9614ba7768623a3826de9ca97b1f75fa025233
shuimuliang <shuimuliang@gmail.com>
2023-12-16 10:31:26 +0800

    feat: build FreeRTOS-Rust

--- build.rs
+++ build.rs
@@ -15,7 +15,35 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
+// fn print_env() {
+//     let env_keys = ["TARGET", "OUT_DIR", "HOST"];
+//     env::vars().for_each(|(key, val)| {
+//         if key.starts_with("CARGO") {
+//             println!("cargo:warning={}={}", key, val);
+//         } else if env_keys.contains(&key.as_str()) {
+//             println!("cargo:warning={}={}", key, val);
+//         } else {
+//             // println!("cargo:warning={}={}", key, val);
+//         }
+//     });
+// }
+
 fn main() {
+    // print_env();
+
+    let mut b = freertos_cargo_build::Builder::new();
+
+    // Path to FreeRTOS kernel or set ENV "FREERTOS_SRC" instead
+    b.freertos("/Users/cairo/embed-rs/FreeRTOS-Kernel");
+    b.freertos_config("src");       // Location of `FreeRTOSConfig.h` 
+    b.freertos_port("GCC/ARM_CM3".to_string()); // Port dir relativ to 'FreeRTOS-Kernel/portable' 
+    // b.heap("heap_4.c".to_string());             // Set the heap_?.c allocator to use from 
+                                    // 'FreeRTOS-Kernel/portable/MemMang' (Default: heap_4.c)       
+
+    // b.get_cc().file("More.c");   // Optional additional C-Code to be compiled
+
+    b.compile().unwrap_or_else(|e| { panic!("{}", e.to_string()) });
+
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -40,4 +68,7 @@ fn main() {
 
     // Set the linker script to the one provided by cortex-m-rt.
     println!("cargo:rustc-link-arg=-Tlink.x");
+
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,17 @@
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
 
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32h7xx_hal::{pac, prelude::*};
+
+use freertos_rust::*;
+
+#[global_allocator]
+static GLOBAL: FreeRtosAllocator = FreeRtosAllocator;
 
 #[entry]
 fn main() -> ! {
@@ -21,4 +27,16 @@ fn main() -> ! {
     loop {
         // 延时500ms
     }
+}
+
+// #[alloc_error_handler]
+// fn alloc_error(_layout: Layout) -> ! {
+//     //set_led(true);
+//     asm::bkpt();
+//     loop {}
+// }
+
+#[no_mangle]
+fn vApplicationStackOverflowHook(pxTask: FreeRtosTaskHandle, pcTaskName: FreeRtosCharPtr) {
+    asm::bkpt();
 }
